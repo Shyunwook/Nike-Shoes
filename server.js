c1280da0af28140c9de231ec80aaa98361a02fd5
wook\신현욱 <gusdnr202@naver.com>
2018-06-26 23:31:52 +0900

    fail...

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get('/',function(req,res){
 
 app.get('/kakaologin',function(req,res){
   var code = req.query.code;
+
   getToken(code,function(){
     res.render('main.ejs',{'code':code});
   });
@@ -46,32 +47,17 @@ app.get('/kakaologin',function(req,res){
 
 function getToken(code,callback){
   console.log('실행');
-  var json = '{"grant_type":"authorization_code","client_id":"5cb4b52a3aed06734662d776fdd9dc0d","redirect_uri":"http://localhost:3000/kakaologin","code":code}';
+  var json = '{"grant_type":"authorization_code","client_id":"5cb4b52a3aed06734662d776fdd9dc0d","redirect_uri":"http://localhost:3000/kakaologin","code":'+code+'}';
   var url = "https://cors-anywhere-esko.herokuapp.com/https://kauth.kakao.com/oauth/token";
-  var headers = {
-    'User-Agent':"Mozilla/5.0 (Windows; U; Windows NT 6.1; ko; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 IPMS/A640400A-14D460801A1-000000426571",
-    'Content-Type' : 'application/x-www-form-urlencoded'
-  };
   request({
     method: 'POST',
     url : url,
     json: true,
-    body: json,
-    headers: headers,
   },function(error,response,data){
-    // if(error){
-    //   console.log(error);
-    // }else if(response.statusCode=="200"){
-    //   console.log('성공이야');
-    //   callback();
-    // }else{
-    //   console.log('???'+response.statusCode);
-    //   console.log(error);
-    //   callback();
-    // }
+
     console.log(error);
-    console.log(response.statusCode,response.headers);
-    console.log(data);    
+    console.log(response.statusCode);
+    console.log(data);
   });
 }
 
